c5f58c41287207ad404df2e869067bf763ac8cbc
Laurence NG <png8@gatech.edu>
2020-02-10 17:55:16 +0000

    used timer and gpio lib in kern

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -18,8 +18,19 @@ use console::kprintln;
 
 // FIXME: You need to add dependencies here to
 // test your drivers (Phase 2). Add them as needed.
+use core::time::Duration;
+use pi::timer;
+use pi::gpio::{Gpio, Function};
 
-fn kmain() -> ! {
-    // FIXME: Start the shell.
-    unimplemented!()
+unsafe fn kmain() -> ! {
+    // FIXME: STEP 1: Set GPIO Pin 16 as output.
+    let mut led = Gpio::new(16).into_output();
+
+    // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        led.set();
+        timer::spin_sleep(Duration::from_secs(2));
+        led.clear();
+        timer::spin_sleep(Duration::from_secs(1));
+    }
 }
