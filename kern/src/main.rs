782d6ba23e8f786d65690981783748e56b06b925
Laurence NG <png8@gatech.edu>
2020-02-14 17:15:18 +0000

    main updated

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -21,16 +21,29 @@ use console::kprintln;
 use core::time::Duration;
 use pi::timer;
 use pi::gpio::{Gpio, Function};
+use pi::uart::*;
 
 unsafe fn kmain() -> ! {
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
-    let mut led = Gpio::new(16).into_output();
+    
+    // let mut led = Gpio::new(16).into_output();
+    // let mut led2 = Gpio::new(20).into_output();
+    // let mut led3 = Gpio::new(21).into_output();
+    let mut uart = MiniUart::new();
 
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
     loop {
-        led.set();
-        timer::spin_sleep(Duration::from_secs(2));
-        led.clear();
-        timer::spin_sleep(Duration::from_secs(1));
+        // led.set();
+        // timer::spin_sleep(Duration::from_millis(100));
+        // led.clear();
+        // timer::spin_sleep(Duration::from_millis(100));
+        // led2.set();
+        // led3.set();
+        // timer::spin_sleep(Duration::from_millis(100));
+        // led2.clear();
+        // led3.clear();
+        // timer::spin_sleep(Duration::from_millis(100));
+        let b = uart.read_byte();
+        uart.write_byte(b);
     }
 }
