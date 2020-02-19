79cfadb1a92e638fb928cac493835980130694b3
Laurence NG <png8@gatech.edu>
2020-02-19 00:45:08 +0000

    panic done

--- kern/src/init/panic.rs
+++ kern/src/init/panic.rs
@@ -1,6 +1,25 @@
 use core::panic::PanicInfo;
+use crate::console::kprintln;
+use crate::console::kprint;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    kprintln!();
+    kprintln!();
+    kprintln!("     .-''''^'''^''^'''^''-.");
+    kprintln!("    (//\\\\//\\\\//\\\\//\\\\//\\\\//)");
+    kprintln!("     ~\\\\^^^^^^^^^^^^^^^^//~");
+    kprintln!("       `================`");
+    kprintln!();
+    kprintln!("      Lamb sauce is missing");
+    kprintln!();
+    kprintln!("------------- PANIC -------------");
+    kprintln!();
+    kprintln!("FILE: {}", _info.location().expect("").file());
+    kprintln!("LINE: {}", _info.location().expect("").line());
+    kprintln!("COL: {}", _info.location().expect("").column());
+    kprintln!();
+    kprintln!("{:?}", _info.message());
+
     loop {}
 }
