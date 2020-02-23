9841c71329c60d3f5ffd3747d25abc1fb23c3fa6
Laurence NG <png8@gatech.edu>
2020-02-23 21:56:02 +0000

    main for atags

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -7,6 +7,7 @@
 #![feature(raw_vec_internals)]
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
+#![feature(panic_info_message)]
 
 #[cfg(not(test))]
 mod init;
@@ -28,11 +29,21 @@ use fs::FileSystem;
 #[cfg_attr(not(test), global_allocator)]
 pub static ALLOCATOR: Allocator = Allocator::uninitialized();
 pub static FILESYSTEM: FileSystem = FileSystem::uninitialized();
+use pi::atags::Atags;
 
 fn kmain() -> ! {
-    unsafe {
-        ALLOCATOR.initialize();
-        FILESYSTEM.initialize();
+    // unsafe {
+    //     ALLOCATOR.initialize();
+    //     FILESYSTEM.initialize();
+    // }
+
+    let mut a = Atags::get();
+    loop {
+        match a.next() {
+            Some(b) => kprintln!("{:#?}", b),
+            None => break,
+        }
+        
     }
 
     kprintln!("Welcome to cs3210!");
