5f4ff81de9203141797ce9b390bad2101f99e39a
Laurence NG <png8@gatech.edu>
2020-02-24 02:04:17 +0000

    align up down done

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,7 +6,21 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    let mut align_value = align;
+    loop {
+        if align_value < 2 {
+            panic!("`align` is not a power of 2.");
+        }
+        else if align_value == 2 {
+            break;
+        }
+        else {
+            align_value /= 2;
+        }
+    }
+    let reminder = addr % align;
+    let aligned_addr = addr - reminder;
+    return aligned_addr;
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -18,5 +32,25 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    let mut align_value = align;
+    loop {
+        if align_value < 2 {
+            panic!("`align` is not a power of 2.");
+        }
+        else if align_value == 2 {
+            break;
+        }
+        else {
+            align_value /= 2;
+        }
+    }
+    let reminder = addr % align;
+    let mut aligned_addr = addr;
+    if reminder > 0 {
+        aligned_addr = addr + align - reminder;
+    }
+    if aligned_addr < addr {
+        panic!("aligning up overflows the address.");
+    }
+    return aligned_addr;
 }
